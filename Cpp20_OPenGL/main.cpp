67fdb1375472b1b5d93171fe434b2d089de14f75
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2024-02-14 10:51:05 +0100

    opengl

--- Cpp20_OPenGL/main.cpp
+++ Cpp20_OPenGL/main.cpp
@@ -1,38 +1,60 @@
 /* OpenGL C++ 20 Glew and Glfw Template */
-#include <GLFW/glfw3.h>
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
 
-int main(void)
-{
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
+const GLint WinWidth = 640;
+const GLint WinHeight = 480;
+int main()
+{  
+    if (!glfwInit()){
         return -1;
+    }
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    // setup window properties
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // core profile = no backward compatibility
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow* mainWindow = glfwCreateWindow(WinWidth, WinHeight, "Test World", NULL, NULL);
+    
+    if (!mainWindow)
     {
         glfwTerminate();
         return -1;
     }
+    // get buffer size information (is the part of the window to draw)
+    int bufferWidth;
+    int bufferHeight;
+    glfwGetFramebufferSize(mainWindow,&bufferWidth,&bufferHeight);
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    // set context for glew to use (this allow to switch between windows)
+    glfwMakeContextCurrent(mainWindow);
+    
+    // Allow modern extension features
+    glewExperimental = GL_TRUE;
+    // check for glew to init
+    if(glewInit() != GLEW_OK){
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return -1;
+    }
+    // Setup viewport size (THE VIEW)
+    glViewport(0,0,bufferWidth,bufferHeight);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+
+    while (!glfwWindowShouldClose(mainWindow))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
+        // inputs events
         glfwPollEvents();
+        // clear window (from 0 to 1 RGBA)
+        glClearColor(0.3f,0.5f,1.0f,1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        // 2 buffers at this moment.
+        glfwSwapBuffers(mainWindow);
     }
 
-    glfwTerminate();
+    //glfwTerminate();
     return 0;
 }
